7fbc9ecf814b7b06a4ff271a48c5f8acdf35d0f1
Mike Shoemake <atlantatechie@gmail.com>
2012-11-08 10:29:33 -0500

    Finished maven integration and ant build.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 c:
-cd \work\shoemake-home\Home
+cd \work\shoemake-home
 REM SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_32
 SET JAVA_HOME=C:\Program Files (X86)\Java\jdk1.6.0_18
 SET ANT_HOME=C:\apps\apache-ant-1.8.4
